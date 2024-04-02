0956ec1c36f0fe16d6a52ddace7760dd076cf864
Schimiuss <schimiuss@gmail.com>
2024-04-02 15:00:12 +0200

    first commit

---
+++ src/main/java/com/project/persWebsite/PersWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.project.persWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/persWebsite/PersWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.persWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
